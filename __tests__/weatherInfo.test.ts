9287c13f995a7445c57e562f71b5490b44cb5cd5
Junpei-Nakasone <nakasone.junpei0719@gmail.com>
2023-01-01 06:13:58 +0900

    add testcase for WeatherInfo class property

--- __tests__/weatherInfo.test.ts
+++ __tests__/weatherInfo.test.ts
@@ -1,9 +1,30 @@
 import { WeatherInfo } from "../src/weatherInfo";
 
 describe('WeatherInfo', () => {
-  it('exists', () => {
+  test('WeatherInfo instance exists', () => {
     const weatherInfo = new WeatherInfo()
     expect(weatherInfo).toBeDefined();
+  })
+
+  test('WeatherInfo should has apiKey and apiURL property', () => {
+    const weatherInfo = new WeatherInfo()
+    expect(weatherInfo).toHaveProperty("apiURL")
+  })
+
+  test('WeatherInfo should has fetchAPI method', () => {
+    expect(1).toStrictEqual(1)
+  })
+
+  test('fetchAPI mehtod should set correct param from received arguments', () => {
+    expect(1).toStrictEqual(1)
+  })
+
+  test('a', () => {
+    expect(1).toStrictEqual(1)
+  })
+
+  it('a', () => {
+    expect(1).toStrictEqual(1)
   })
 })
 
--- src/weatherInfo.ts
+++ src/weatherInfo.ts
@@ -1,3 +1,6 @@
 export class WeatherInfo {
+  apiKey: string
+  // https://api.openweathermap.org/data/2.5/weather?q=London&appid={API key}
+  apiURL: string = "https://api.openweathermap.org/data/2.5/weather?=Okinawa&appid="
 
 }
